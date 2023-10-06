c2d2d35e2637452cbb7ced5f9850c29e2642ca8e
jjuuuunnii <jake991110@naver.com>
2023-10-07 02:09:53 +0900

    Fix: /userInfo로 변경

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
-                <Route path="/user-info" element={<MoreInfoPage />} />
+                <Route path="/userInfo" element={<MoreInfoPage />} />
                 <Route path="/chat" element={<ChatPage />} />
             </Routes>
         </Router>
